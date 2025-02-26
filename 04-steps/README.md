959669f9456998ab627a8d2b0b1bd1acf72401ad
mviersel <119597405+mviersel@users.noreply.github.com>
2025-02-26 16:42:13 +0100

    Children "props" toevoegen

--- 04-steps/README.md
+++ 04-steps/README.md
@@ -86,3 +86,48 @@ setStep((s) => +1);
 ```
 
 Hier pak je de functie _setSetp_ die een value mee krijgt, _step_. In de fuctie geeft je _step_ mee als _s_. Die vervolg je met een arrow function en vertel je dat er _+1_ bij moet.
+
+# Versie 2 - Children
+
+Het maken van components is het makkelijke aan werken met React. Het scheelt zoveel tijd, maar je moet het eerst wel goed opzetten. Hier zijn twee verschillende manier te zien om hetzelfde te bereiken:
+
+```js
+<Button
+  textColor="fff"
+  bgColor="#7950f2"
+  onClick={handlePrevious}
+  text="Previous"
+  emoji="👈"
+/>
+```
+
+Je kunt alle informatie die die knop moet hebben als props meegeven, maar dan kan je component snel vollopen.
+
+```js
+<Button textColor="fff" bgColor="#7950f2" onClick={handleNext}>
+  Next<span>👉</span>
+</Button>
+```
+
+Je kunt ook children gebruiken. Hiermee kun je de text die anders met props mee werd gegeven gewoon als jsx meegeven. zo kun je in dit geval de emoji's aan de kant zetten waar je die wil hebben zonder hiervoor extra props mee te geven.
+
+## Button voorbeeld
+
+```js
+<StepMessage step={step}>{messages[step - 1]}</StepMessage>
+```
+
+Hier wordt de component aangroepen. De `step` en `children` worden hier meegegeven.
+
+```js
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+```
+
+Deze twee stukjes code zorgen samen voor een werkend bericht. Om de component `function StepMessage({step, children})` twee props meekrijgt kan het bericht zichzelf gewoon invullen. **De children is altijd de text die tussen het beign en het einde van de component zet**.
--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -9,12 +9,12 @@ const messages = [
 export default function App() {
   return <div>
     <Steps />
-    {/* <Steps /> */}
+    <Steps />
   </div>
 }
 
 function Steps() {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
@@ -40,34 +40,19 @@ function Steps() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          {/* <p className="message">
-            <h3>Step {step}: {messages[step - 1]}</h3>
-          </p> */}
-
-          <StepMessage step={step}>
-            {messages[step - 1]}
-          </StepMessage>
+          <p className="message">Step {step}: {messages[step - 1]}</p>
 
           <div className="buttons">
-            <Button textColor="fff" bgColor="#7950f2" onClick={handlePrevious}><span>👈</span>Previous</Button>
-            <Button textColor="fff" bgColor="#7950f2" onClick={handleNext}>Next<span>👉</span></Button>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
+              Next
+            </button>
           </div>
 
         </div>
       )}
     </div>
   );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>{children}</button>
 }
